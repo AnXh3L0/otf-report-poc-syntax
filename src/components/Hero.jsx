f7f13a26427e870aff769d01955184fbcc88b6ca
Anxhelo Lushka <anxhelo1995@gmail.com>
2024-07-22 22:39:48 +0200

    Changed hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,12 +14,12 @@ function TrafficLightsIcon(props) {
 
 export function Hero() {
   return (
-    <div className="overflow-hidden bg-slate-50 dark:bg-slate-900 dark:-mb-32 dark:mt-[-4.75rem] dark:pb-32 dark:pt-[4.75rem]">
+    <div className="overflow-hidden bg-slate-100 dark:bg-slate-900 dark:-mb-32 dark:mt-[-4.75rem] dark:pb-32 dark:pt-[4.75rem]">
       <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
           <div className="relative z-10 md:text-center lg:text-left">
             <div className="relative">
-              <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+              <p className="inline bg-gradient-to-r from-indigo-400 via-sky-700 to-indigo-400 bg-clip-text font-display text-5xl tracking-tight text-transparent">
                 Open Technology Fund Annual Report 2022
               </p>
               <p className="mt-3 text-2xl tracking-tight text-slate-400">
